5da9f8712fd8a7bfd7ff99bd3d985dfe0f8a199a
Bálint János Gergely <balintjanosgergely@gmail.com>
2024-03-21 13:42:13 +0100

    Revert SigningAgent

--- Remote/net/balintgergely/remote/certificates/CertificateMaker.java
+++ Remote/net/balintgergely/remote/certificates/CertificateMaker.java
@@ -6,7 +6,10 @@ import java.nio.ByteBuffer;
 import java.security.GeneralSecurityException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.security.spec.MGF1ParameterSpec;
@@ -26,38 +29,30 @@ import net.balintgergely.remote.security.asn1.Asn1Integer;
 import net.balintgergely.remote.security.asn1.Asn1Item;
 import net.balintgergely.remote.security.asn1.Asn1RawItem;
 import net.balintgergely.remote.security.asn1.Asn1Sequence;
+import net.balintgergely.remote.security.asn1.Asn1Support;
+import net.balintgergely.remote.security.asn1.Asn1Support.AlgorithmId;
 
 import static net.balintgergely.remote.security.asn1.Asn1ContextSequence.wrap;
 
 public class CertificateMaker {
+	@SuppressWarnings("unchecked")
+	private static final <E extends Throwable> void throwUnchecked(Throwable t) throws E{ throw (E)t; }
 	private AtomicReference<BigInteger> serialNumber = new AtomicReference<BigInteger>(BigInteger.ZERO);
 	private X500Principal myIdentity;
 	private Asn1Item myIdentityEncoded;
-	private SigningAgent signingAgent;
-	public CertificateMaker(X500Principal myIdentity,SigningAgent signingAgent){
+	private Asn1Item privateKeyAlgorithmId;
+	private AlgorithmId parsedAlgorithmId;
+	private PrivateKey privateKey;
+	public CertificateMaker(X500Principal myIdentity,PrivateKey privateKey){
 		this.myIdentity = myIdentity;
 		this.myIdentityEncoded = new Asn1RawItem(ByteBuffer.wrap(this.myIdentity.getEncoded()));
-		this.signingAgent = signingAgent;
-	}
-	public static void main(String[] atgs) throws Exception{
-		KeyPairGenerator keygen = KeyPairGenerator.getInstance("PSS");
-		PSSParameterSpec parameterSpec = new PSSParameterSpec(
-			"SHA256",
-			"MGF1",
-				new MGF1ParameterSpec("SHA256"), 20, 1);
-		RSAKeyGenParameterSpec keygenSpec = new RSAKeyGenParameterSpec(
-			4096, RSAKeyGenParameterSpec.F4, parameterSpec);
-		keygen.initialize(keygenSpec);
-		KeyPair myKeyPair = keygen.generateKeyPair();
-		PrivateKeyAgent signAgent = new PrivateKeyAgent(myKeyPair.getPrivate());
-		//makeCertificate(myKeyPair,myKeyPair.getPrivate());
-		X509Certificate cert = new CertificateMaker(
-			new X500Principal("CN=Test issuer"),
-			signAgent).makeX509Certificate(
-			new X500Principal("CN=Test subject"),
-			myKeyPair.getPublic(),
-			OffsetDateTime.now().plusHours(1));
-		cert.verify(myKeyPair.getPublic());
+		this.privateKey = privateKey;
+		Asn1Sequence issuerKeyEncoded = new Asn1RawItem(
+			ByteBuffer.wrap(privateKey.getEncoded())).as(Asn1Sequence.class);
+		privateKeyAlgorithmId = issuerKeyEncoded.items().get(1)
+			.as(Asn1RawItem.class).duplicate()
+			.as(Asn1Sequence.class);
+		parsedAlgorithmId = Asn1Support.parseSignatureAlgorithmId(privateKeyAlgorithmId);
 	}
 	public X509Certificate makeX509Certificate(X500Principal subject,PublicKey subjectKey,Temporal validUntil) throws GeneralSecurityException, IOException{
 		// https://www.rfc-editor.org/rfc/rfc5280
@@ -74,7 +69,6 @@ public class CertificateMaker {
 			new Asn1GeneralizedTime(validUntil)
 		);
 		BigInteger serial = serialNumber.updateAndGet(BigInteger.ONE::add);
-		Asn1Item privateKeyAlgorithmId = signingAgent.getAlgorithmId();
 		Asn1Item tbs = new Asn1Sequence(
 			wrap(0x80,new Asn1Integer(1)), // Version number
 			new Asn1Integer(serial),
@@ -86,16 +80,44 @@ public class CertificateMaker {
 		);
 		Asn1Collector tbsCollector = new Asn1Collector();
 		tbs.writeTo(tbsCollector);
-		byte[] signature = signingAgent.sign(tbsCollector.stream().spliterator());
+		Signature sign = Asn1Support.createSignature(parsedAlgorithmId);
+		sign.initSign(privateKey);
+		tbsCollector.stream().forEach(t -> {
+			try{
+				sign.update(t);
+			}catch(SignatureException e){
+				throwUnchecked(e);
+			}
+		});
 		Asn1Collector rootCollector = new Asn1Collector();
 		Asn1Item root = new Asn1Sequence(
 			tbs,
 			privateKeyAlgorithmId,
-			new Asn1BitString(signature, 0)
+			new Asn1BitString(sign.sign(), 0)
 		);
 		root.writeTo(rootCollector);
 		CertificateFactory cf = CertificateFactory.getInstance("X.509");
 		X509Certificate cert = (X509Certificate)cf.generateCertificate(rootCollector.toInputStream());
 		return cert;
+	}
+	public static void main(String[] atgs) throws Exception{
+		KeyPairGenerator keygen = KeyPairGenerator.getInstance("PSS");
+		PSSParameterSpec parameterSpec = new PSSParameterSpec(
+			"SHA256",
+			"MGF1",
+				new MGF1ParameterSpec("SHA256"), 20, 1);
+		RSAKeyGenParameterSpec keygenSpec = new RSAKeyGenParameterSpec(
+			4096, RSAKeyGenParameterSpec.F4, parameterSpec);
+		keygen.initialize(keygenSpec);
+		KeyPair myKeyPair = keygen.generateKeyPair();
+		//makeCertificate(myKeyPair,myKeyPair.getPrivate());
+		X509Certificate cert = new CertificateMaker(
+			new X500Principal("CN=Test issuer"),
+			myKeyPair.getPrivate()).makeX509Certificate(
+			new X500Principal("CN=Test subject"),
+			myKeyPair.getPublic(),
+			OffsetDateTime.now().plusHours(1));
+		cert.verify(myKeyPair.getPublic());
+		System.out.println("Done.");
 	}
 }
